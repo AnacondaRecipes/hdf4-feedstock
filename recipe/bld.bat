70f28cbbda9d10d51dba5bff6892d4213212fb3b
vkhomits <khomitsvalia@gmail.com>
2026-06-03 14:42:32 +0300

    Rebuild against libjpeg-turbo

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,10 +5,18 @@ set INCLUDE=%LIBRARY_INC%;%INCLUDE%;%RECIPE_DIR%
 mkdir build_dir
 cd build_dir
 
+:: Moving changes from patch here to avoid errors with patching on windows. 
+:: Files have a different line ending and patch doesn't work well with that.
+python -c "
+content = open('hdf/src/hlimits.h').read()
+content = content.replace('#   define MAX_FILE   32', '#   define MAX_FILE   4096')
+open('hdf/src/hlimits.h', 'w').write(content)
+"
+
 :: Configure step.
 cmake -G "%CMAKE_GENERATOR%" ^
       -D CMAKE_BUILD_TYPE=Release ^
-      :: -D HDF4_BUILD_HL_LIB=ON ^
+      -D CMAKE_POLICY_VERSION_MINIMUM=3.5 ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D ZLIB_DIR=%LIBRARY_PREFIX% ^
       -D JPEG_DIR=%LIBRARY_PREFIX% ^
